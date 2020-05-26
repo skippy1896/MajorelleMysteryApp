db2a72729793b9815d37ce976afd852b2c3bccad
Joerg Reichert <de.abg.reichert.joerg@googlemail.com>
2020-05-27 00:04:41 +0200

    initial Godot project structure

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module majorellemystery {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires javafx.swing;
-    requires javafx.media;
-
-    exports eu.cdvsll20.majorellemystery;
-}
